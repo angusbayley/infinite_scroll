bf8272ffc1c7729d01027a76d89b0593810e6574
baills <angusbayley@gmail.com>
2014-04-21 17:52:28 +0100

    first working version

--- is.js
+++ is.js
@@ -2,18 +2,26 @@ window.onload = function() {
 	//	TEST:  //	document.getElementById('first').innerHTML = "first line";
 };
 
+
+window.onscroll = function() {
+	if (window.scrollY + window.innerHeight >= document.body.offsetHeight) {
+		retrieve();
+	}
+}
+
 xhr = new XMLHttpRequest();
 
 function retrieve() {
 	xhr.open("get", "extraData.txt", true);
 	xhr.send(null);
-
 }
 
 xhr.onreadystatechange = function() {
 	if (xhr.readyState === 4) {
 		if (xhr.status >= 200 && xhr.status < 300 || xhr.status === 304) {
-			alert("Response received");
+			//alert("Response received");
+			document.body.innerHTML += xhr.responseText;
+			console.log("should have been updated now");
 		}
 		else {
 			alert("No response received");
